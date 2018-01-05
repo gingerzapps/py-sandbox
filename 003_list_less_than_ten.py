48902203b5e36956ce75b827e42e7ac8f906ea97
gingerzapps <gingerzapps@users.noreply.github.com>
2018-01-05 14:42:28 -0700

    Update 003_list_less_than_ten.py

--- 003_list_less_than_ten.py
+++ 003_list_less_than_ten.py
@@ -9,43 +9,27 @@
 #            Ask user for a  number, return all elements from original list that are smaller than that number
 
 
-a = [10, 6, 15, 64, 88, 32, 22, 8, 9, 44, 7, 3, 5, 1, 3, 15, 64]
+##a = [10, 6, 15, 64, 88, 32, 22, 8, 9, 44, 7, 3, 5, 1, 3, 15, 64]
 
-##a = []
-##looping = 'true'
-##while looping == 'true':
-##    apnd = input("Give me an integer, or enter 'N/n' to complete list:")
-##    if (len(a) < 2  and (apnd == "" or (apnd == "N" or apnd == "n"))):
-##        print("You must add at least two integers to the list. Please try again.")
-##    elif apnd == "N" or apnd == "n":
-##        looping = 'false'
-##    elif apnd == "":
-##        print("Oops, try again. Don't enter a blank line.")
-##    else:
-##        a.append(int(apnd))
-##print (a)
+a = []
+looping = 'true'
+while looping == 'true':
+    apnd = input("Give me an integer, or enter 'N/n' to complete list:")
+    if (len(a) < 2  and (apnd == "" or (apnd == "N" or apnd == "n"))):
+        print("You must add at least two integers to the list. Please try again.")
+    elif apnd == "N" or apnd == "n":
+        looping = 'false'
+    elif apnd == "":
+        print("Oops, try again. Don't enter a blank line.")
+    else:
+        a.append(int(apnd))
+print (a)
 
 #Check for duplicates, add them to a new list (use "set(your_list)" to remove all duplicates from list. Good for relatively short lists)
 print("I will make a set out of the following list: ")
 print(a)
 a = list(set(a))
 print(a)
-
-#Remove elements larger than 10
-##for i in range(len(a)):
-##    print("++++++++++++++++++++++++++++++++++")
-##    print("i = " + str(i) + " ||| len(a) = " + str(len(a)) + " ||| elem = " + str(a[i]))
-##    print(a)
-##    if int(a[i]) >= 10:
-##        print("The element " + str(a[i]) + " has been removed.")
-##        a.remove(a[i])
-##        print(a)
-##        print("Current i value = " + str(i))
-##        i -= 1
-##        print("Depricated i value = " + str(i))
-##        if i == len(a):
-##            break;
-##    print("\n=\n=")
 
 #Remove elements larger than 10
 #Using while instead of for loop because for loop has restricted control on index iterablei = 0
