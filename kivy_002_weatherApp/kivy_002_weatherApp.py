3aa4568b399f7e1ec437ad1894b77bdaa68466af
gingerzapps <gingerzapps@users.noreply.github.com>
2018-01-06 12:46:30 -0700

    Update kivy_002_weatherApp.py

--- kivy_002_weatherApp/kivy_002_weatherApp.py
+++ kivy_002_weatherApp/kivy_002_weatherApp.py
@@ -6,13 +6,16 @@
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 class WeatherApp(App):
     pass
 
 class AddLocationForm(BoxLayout):
-    def search_location(self):
-        print("Explicit is better than implicit.")
+    search_input = ObjectProperty() #search_input property is created at class level as instance of ObjectProperty class
+    
+    def search_location(self): #can print to console, this is called when button is pressed (defined in .kv file)
+        print("The user searched for '{}'".format(self.search_input.text)) 
 
 if __name__ == "__main__":
     WeatherApp().run()
