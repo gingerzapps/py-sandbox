65641404b3a3dcde4fd89ea22c9744b8fb9b01a0
gingerzapps <gingerzapps@users.noreply.github.com>
2018-01-06 23:50:17 -0700

    Update kivy_002_weatherApp.py
    Added capability to update list of cities in window based on user search. Uses Open Weather Map API

--- kivy_002_weatherApp/kivy_002_weatherApp.py
+++ kivy_002_weatherApp/kivy_002_weatherApp.py
@@ -3,10 +3,12 @@
 #End result will be a Weather App which can display C or F, search locations, store locations between
 #       app instances, use device GPS to select current location
 #Chapter 1 complete 01-05-2018
+#Chapter 2 complete 01-06-2018
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+from kivy.network.urlrequest import UrlRequest
 
 class WeatherApp(App):
     pass
@@ -15,25 +17,40 @@ class AddLocationForm(BoxLayout):
     search_input = ObjectProperty() #search_input property is created at class level as instance of ObjectProperty class
     
     def search_location(self): #can print to console, this is called when button is pressed (defined in .kv file)
-        print("The user searched for '{}'".format(self.search_input.text)) 
+        print("The user searched for '{}'".format(self.search_input.text))
+        search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like&APPID=" + "YOUR_API_KEY" #free account, get api key from open weather map
+        search_url = search_template.format(self.search_input.text)
+        request = UrlRequest(search_url, self.found_location, self.redirect, self.fail, self.err, self.progress)
+
+        
+    def found_location(self, request, data):
+        cities = ["{} ({})".format(d['name'], d['sys']['country'])
+                  for d in data['list']]
+        self.search_results.item_strings = cities
+        
+    #debugging functions that mark if something unusual happened with UrlRequest above
+    def redirect(self, request, data):
+        print("request Redirected")
+        print(str(request))
+        print(str(data))
+
+    def fail(self, request, data):
+        print("request Failed")
+        print(str(request))
+        print(str(data))
+
+    def err(self, request, data):
+        print("request Error")
+        print(str(request))
+        print(str(data))
+
+    def progress(self, request, data, extra):
+        print("request Progress")
+        print(str(request))
+        print(str(data))
+        print(str(extra))
+    
+        
 
 if __name__ == "__main__":
     WeatherApp().run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
